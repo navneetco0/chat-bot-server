b73a4dd038509bcfd26b2bad350b0b64d2983e5e
navneetco0 <navneetharsh3@gmail.com>
2022-09-24 03:06:44 +0530

    ressolve uri

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
 const mongoose = require('mongoose');
 
 module.exports=()=>{
-    mongoose.connect(`mongodb+srv://navneetfw14:Navnee7250@cluster0.iowwj.mongodb.net/realtimechat?retryWrites=true&w=majority`);
+    mongoose.connect(`mongodb+srv://navneetfw14:${process.env.URI}@cluster0.iowwj.mongodb.net/realtimechat?retryWrites=true&w=majority`);
 }
