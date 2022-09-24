e2b118d806001871f2dc0461b1d078c2382bf74a
navneetco0 <navneetharsh3@gmail.com>
2022-09-24 05:56:26 +0530

    working fine

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const connect = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 const upload = require('./helpers/filehelper');
 const {login, register} = require('./controllers/user.controller');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.post('/login', login);
 app.post('/register',   upload.single('file'), register);
 
