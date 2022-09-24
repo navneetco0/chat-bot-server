f84b57533a24d1c3a134918e46fa0e71974de75c
navneetco0 <navneetharsh3@gmail.com>
2022-09-24 11:43:44 +0530

    working fine

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const jwt = require('jsonwebtoken')
+const User = require('../models/User.model');
 const router = express.Router()
 
 router.post('/', async (req, res) => {
