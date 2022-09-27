7af4a0b173b9d45b065b51e97d71c3cf13645e73
navneetco0 <navneetharsh3@gmail.com>
2022-09-27 21:42:15 +0530

    fiximg cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 
