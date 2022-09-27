d3b215bc2b7d083856d5314735eef0fcb9e3017d
navneetco0 <navneetharsh3@gmail.com>
2022-09-27 23:13:04 +0530

    working on cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:"https://chat-bot-dim1.vercel.app/*",
+        origin:"https://chat-bot-dim1.vercel.app/",
         methods:["GET", "POST"],
     }
 })
